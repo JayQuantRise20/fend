681415692567e60dc126dd859378f5071e473b12
Javad Gholizadeh <javad.gholizadeh@veritasadvisory.co.uk>
2022-05-27 11:10:18 +0100

    project started

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,12 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+var aylien = require("aylien_textapi");
+
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+var textapi = new aylien({
+    application_key: process.env.API_KEY
+ });
+
+
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+// You could call it aylienapi, or anything else
+
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
